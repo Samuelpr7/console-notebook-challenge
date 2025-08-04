2c43c2b51aab7467fcfa6482bc5a3768394eecb7
b14s210 <samuelitopr77@gmail.com>
2025-08-04 17:45:00 -0500

    Avance de codigo #1

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,29 @@
-# TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class NOTA:
+    HIGH = 'HIGH'
+    MEDIUM = 'MEDIUM'
+    LOW = 'LOW'
+
+    def __init__(self, code: str, tittle: str, text: str, importance: str) -> None:
+        self.code = code
+        self.tittle = tittle
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.today()
+        self.tags = []
+
+        def add_tag(self, tag: str):
+            self.tags.append(tag)
+
+        def __str__(self):
+            return f"date: {self.creation_date}, tittle: {self.tittle}, text: {self.text}"
+
+class Notebook:
+    def __init__(self):
+        self.list = []
+
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        def generar_nuevo_codigo(notas):
+            codigos_existentes = {nota['codigo'] for nota in notas}
+            nuevo_codigo = len(notas) + 1
